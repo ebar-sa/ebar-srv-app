4aecb02ffade5922b9a99b684156bee880d0739c
Jorge Raposo <jorrapdia@alum.us.es>
2021-04-01 03:58:09 +0200

    Empezados tests del login.

---
+++ src/test/java/com/ebarapp/ebar/controller/AuthControllerTests.java
@@ -0,0 +1,93 @@
+package com.ebarapp.ebar.controller;
+
+import com.ebarapp.ebar.configuration.security.jwtConfiguration.JwtUtils;
+import com.ebarapp.ebar.configuration.security.payload.request.LoginRequest;
+import com.ebarapp.ebar.model.User;
+import com.ebarapp.ebar.model.type.RoleType;
+import com.ebarapp.ebar.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.TestingAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.hamcrest.Matchers.*;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = AuthController.class,
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfigurer.class),
+        excludeAutoConfiguration = SecurityAutoConfiguration.class)
+class AuthControllerTests {
+
+    private static final String USERNAME = "pepediaz";
+    private static final String PASSWORD = "1234pepe";
+    private static final String EMAIL = "pepediaz@outlook.com";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private PasswordEncoder encoder;
+
+    @MockBean
+    private JwtUtils jwtUtils;
+
+    @MockBean
+    private UserService userService;
+
+    @MockBean
+    private AuthenticationManager authenticationManager;
+
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        Set<RoleType> roles = new HashSet<>();
+        roles.add(RoleType.ROLE_CLIENT);
+
+        user = new User();
+        user.setUsername(USERNAME);
+        user.setFirstName("Pepe");
+        user.setLastName("Díaz");
+        user.setDni("27485322W");
+        user.setEmail(EMAIL);
+        user.setPhoneNumber("722345789");
+        user.setPassword(PASSWORD);
+        user.setRoles(roles);
+
+        given(this.authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(USERNAME, PASSWORD)))
+                .willReturn(new TestingAuthenticationToken(user, PASSWORD));
+    }
+
+    @Test
+    void testAuthenticateUser() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        LoginRequest request = new LoginRequest();
+        request.setUsername(USERNAME);
+        request.setPassword(PASSWORD);
+        String requestJson = objectMapper.writeValueAsString(request);
+
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/signin").contentType(MediaType.APPLICATION_JSON).content(requestJson))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("username", hasToString(USERNAME)))
+                .andExpect(MockMvcResultMatchers.jsonPath("email", hasToString(EMAIL)));
+    }
+
+}
