734e6e702a0fd2e1f93e1e201fb15ee75bca6fd5
José Ramón Fernández <josferde5@alum.us.es>
2021-04-05 20:12:00 +0200

    Nuevos tests a la aplicación y eliminación de métodos que no se usan

--- src/main/java/com/ebarapp/ebar/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ebarapp.ebar.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ebarapp.ebar.model.User;
-
-public interface UsuarioRepository extends JpaRepository<User, Long>{
-
-}
