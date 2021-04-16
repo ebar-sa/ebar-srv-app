f0536e1e0d95d2a6bc4ae0ffafed3b6e54750baa
Iván Santos Domínguez <ivansd.99@gmail.com>
2021-04-17 00:15:04 +0200

    Arreglado mal olor de import no usado

--- src/main/java/com/ebarapp/ebar/controller/BarTableController.java
+++ src/main/java/com/ebarapp/ebar/controller/BarTableController.java
@@ -5,7 +5,6 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-import com.ebarapp.ebar.service.BarService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
